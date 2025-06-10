1795419d189c6010ff52f6fbe3a574795ea899b9
PaulMakoriMachuka <paulmachukamakori@gmail.com>
2025-06-10 12:15:46 +0300

    my commit

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution in this file!
+// 1) Declare global variable
+var customerName = 'bob';
+
+// 2) Modifies customerName to uppercase
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// 3) Set best customer
+function setBestCustomer() {
+  bestCustomer = 'not bob'; // Implicitly creates a global variable
+}
+
+// 4) Overwrite best customer
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// 5) Attempt to reassign a const (will throw if you try to reassign)
+const leastFavoriteCustomer = 'someone';
+
+// Function that tries to reassign a const (should fail)
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'someone else'; // This will throw an error
+}
+
+module.exports = {
+  customerName,
+  upperCaseCustomerName,
+  setBestCustomer,
+  overwriteBestCustomer,
+  changeLeastFavoriteCustomer
+};
+
